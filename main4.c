9662646fcbc94480f3dfd57ed2e838487d613927
yura <yura.nabokov1990@mail.ru>
2021-12-27 21:20:05 +0400

    first commit

---
+++ main4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+int rebro; int ploschad;
+printf("Enter the length of the edge\n");
+scanf("%d",&rebro);
+ploschad=rebro*rebro;
+printf("Square of the face of the cube is = ");
+printf("%d",ploschad);
+}
+
